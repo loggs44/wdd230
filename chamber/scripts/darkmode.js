8020f038194635bbf42ef874d326232c176a6436
loggsclark <loggsclark44@gmail.com>
2023-10-16 23:30:20 -0600

    dark mode update

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -6,7 +6,8 @@ darkmode.addEventListener('click',()=>{
         document.documentElement.style.setProperty('--text-color', '#E7ECEF');        
         document.documentElement.style.setProperty('--background-color', 'black');    
         document.documentElement.style.setProperty('--hover-background-color', '#27374D');    
-        document.documentElement.style.setProperty('--hover-color', '#E7ECEF');                
+        document.documentElement.style.setProperty('--hover-color', '#E7ECEF');
+        document.documentElement.style.setProperty('--section-background-color', '#232323');                
         bodyelt.style.backgroundColor = 'black';
         darkmode.textContent = 'LIGHT'
     }
@@ -14,7 +15,8 @@ darkmode.addEventListener('click',()=>{
         document.documentElement.style.setProperty('--text-color', 'black');        
         document.documentElement.style.setProperty('--background-color', '#A3CEF1');        
         document.documentElement.style.setProperty('--hover-background-color', '#27374D');    
-        document.documentElement.style.setProperty('--hover-color', '#E7ECEF');        
+        document.documentElement.style.setProperty('--hover-color', '#E7ECEF');
+        document.documentElement.style.setProperty('--section-background-color', '#8B8C89');         
         bodyelt.style.backgroundColor = '#A3CEF1';
         darkmode.textContent = 'DARK'
     }
