a907b95373aef56dadea7e2477bff73ec8111326
loggsclark <loggsclark44@gmail.com>
2023-10-30 21:50:15 -0600

    Discover Page content

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -3,14 +3,14 @@ const DAY_IN_MILLIS = 24*60*60*1000;
 
 // Get site visit information
 function getDiscoverMessage(){
-    let message = "Welcome! Enjoy the landscapes of Story Brooke!"
+    let message = "Welcome! Let us know if you have any questions"
     let lastVisitValue = localStorage.getItem(LAST_VISIT_DATE_KEY)
     let today = new Date();
     if (lastVisitValue != null){
         lastVisit = parseInt(lastVisitValue)
         daysSinceLastVisit = Math.floor((today - lastVisit) / DAY_IN_MILLIS)
         if (daysSinceLastVisit == 0){
-            message = "Glad to see you back so soon!"
+            message = "Back so soon! Awesome!"
         }
         else{
             if (daysSinceLastVisit == 1){
