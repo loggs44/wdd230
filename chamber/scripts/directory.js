9b4a3fb44ce4ed87b7e31d1fddb3bcf467ba5b84
loggsclark <loggsclark44@gmail.com>
2023-11-13 23:01:06 -0700

    Directory Update

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,3 +1,26 @@
+var gridSelector = document.querySelector('#directory-grid');
+var listSelector = document.querySelector('#directory-list');
+var directoryData = document.querySelector('#directory-data');
+
+gridSelector.addEventListener('click', ()=>{
+    if (!gridSelector.classList.contains('active')){    
+        gridSelector.classList.add('active');
+        listSelector.classList.remove('active');
+        directoryData.classList.add('directory-cards')
+        directoryData.classList.remove('directory-list')
+    }
+});
+
+listSelector.addEventListener('click', ()=>{
+    if (!listSelector.classList.contains('active')){
+        listSelector.classList.add('active');
+        gridSelector.classList.remove('active');
+        directoryData.classList.add('directory-list')
+        directoryData.classList.remove('directory-cards')
+    }
+});
+
+
 const url = "./data/members.json";
 
 const displayBusinesses = (businesss) => {
